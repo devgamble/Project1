a6c1be05ca35ceacfa8cf68db8902f95c46e186d
devgamble <dgamble@ucsb.edu>
2019-12-20 10:00:13 -0800

    csv test2

--- randmeantest.R
+++ randmeantest.R
@@ -12,3 +12,10 @@ afunction1 <- function(x){
   if(x>5)
   print(50)
 }
+
+library(here)
+fakedata2 <- read.csv(here("newfolder", "fakedata2.csv"))
+
+fakedata2[101,] <- c(101, NA, NA, NA, FALSE)
+
+write.csv(fakedata2, here("newfolder", "fakedata2.csv"))
